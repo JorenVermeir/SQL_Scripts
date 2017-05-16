ce7c778188221d9cd1921e7675036e8d35c32e9f
arnoldbraine <arnold.braine@gmail.com>
2017-05-16 20:25:07 +0200

    BIG UPDATE

--- DROP-script.sql
+++ DROP-script.sql
@@ -3,14 +3,14 @@ DROP TABLE spikee.team_has_werknemer;
 DROP TABLE spikee.uren;
 DROP TABLE spikee.team_has_task;
 DROP TABLE spikee.fase;
-DROP TABLE spikee.task_has_resources;
-DROP TABLE spikee.resources;
+DROP TABLE spikee.task_has_materials;
+DROP TABLE spikee.materials;
 DROP TABLE spikee.problemen;
-DROP TABLE spikee.klant;
-DROP TABLE spikee.werknemer;
 DROP TABLE spikee.team;
 DROP TABLE spikee.verlof;
 DROP TABLE spikee.task;
 DROP TABLE spikee.project;
+DROP TABLE spikee.klant;
+DROP TABLE spikee.werknemer;
 
 
