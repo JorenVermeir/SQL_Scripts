0446beb5b58b1b5db9e93696884a77be1b22a019
SHIVALEHERO\Shiva <ikhebzwarediaree@hotmail.com>
2017-05-24 13:57:23 +0200

    7

--- QUERY-script.sql
+++ QUERY-script.sql
@@ -1,7 +1,7 @@
 -- 1. In welke fase zit het project en sorteer op volgorde van fases
 
-SELECT fase.stage, project_id, project.naam
-FROM project INNER JOIN fase USING (project_id)
+SELECT fase.stage, project_id
+FROM spikee.project INNER JOIN spikee.fase USING (project_id)
 ORDER BY CASE
 WHEN fase.stage = 'verkoop' THEN '0'
 WHEN fase.stage = 'voorbereiding' THEN '1'
@@ -14,13 +14,13 @@ END
 -- 2. Wat zijn de problemen dat een taak heeft.
 
 SELECT *
-FROM task INNER JOIN problemen USING (task_id)
+FROM spikee.task INNER JOIN spikee.problemen USING (task_id)
 
 -- 3. Tot welke teams behoort een werknemer.
 
 SELECT st.naam, sw.naam ,sw.werknemer_id
-FROM team st INNER JOIN team_has_werknemer sthw ON (st.team_id = sthw.team_id)
-	INNER JOIN werknemer sw ON (sthw.werknemer_id = sw.werknemer_id)
+FROM spikee.team st INNER JOIN spikee.team_has_werknemer sthw ON (st.team_id = sthw.team_id)
+	INNER JOIN spikee.werknemer sw ON (sthw.werknemer_id = sw.werknemer_id)
 ORDER BY sw.naam
 
 -- 4. Welke werknemers zitten in een team.
@@ -30,11 +30,11 @@ ORDER BY sw.naam
 -- 5. Aan welke taken werkt een werknemer en tot welk project behoort deze taak, rangschik alfabetisch op naam van de werknemer.
 
 SELECT swe.naam, st.naam, spr.naam
-FROM task st LEFT JOIN team_has_task stht ON (st.task_id = stht.task_id)
-	INNER JOIN team ste ON (stht.team_id = ste.team_id)
-	INNER JOIN team_has_werknemer sthw ON (ste.team_id = sthw.team_id)
-	INNER JOIN werknemer swe ON (sthw.werknemer_id = swe.werknemer_id)
-	INNER JOIN project spr USING (project_id)
+FROM spikee.task st LEFT JOIN spikee.team_has_task stht ON (st.task_id = stht.task_id)
+	INNER JOIN spikee.team ste ON (stht.team_id = ste.team_id)
+	INNER JOIN spikee.team_has_werknemer sthw ON (ste.team_id = sthw.team_id)
+	INNER JOIN spikee.werknemer swe ON (sthw.werknemer_id = swe.werknemer_id)
+	INNER JOIN spikee.project spr USING (project_id)
 ORDER BY swe.naam
 
 -- 6. Per team weergeven in welke fase het project zit.
@@ -43,14 +43,14 @@ ORDER BY swe.naam
 
 -- 7. Het totaal aantal uren dat een team aan een project heeft gewerkt.
 
+SELECT ste.naam
+FROM spikee.team ste INNER JOIN spikee.team_has_task stht USING (team_id)
+	INNER JOIN spikee.task sta USING (task_id)
+	INNER JOIN spikee.problemen spr USING 	(task_id)
+WHERE max(count(spr.))
 
 
 -- 8. Wat is het team die de meeste problemen heeft.
 
-SELECT ste.naam
-FROM team ste INNER JOIN team_has_task stht USING (team_id)
-	INNER JOIN task sta USING (task_id)
-	INNER JOIN problemen spr USING 	(task_id)
-WHERE max(count(spr.))
 
 -- 9. Welke werknemers werken aan de meeste taken (van hoog naar laag).
