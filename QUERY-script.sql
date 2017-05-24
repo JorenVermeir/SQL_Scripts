9020852aa30efa4faf00ffabf04a809989bdd60e
SHIVALEHERO\Shiva <ikhebzwarediaree@hotmail.com>
2017-05-24 15:24:51 +0200

    10

--- QUERY-script.sql
+++ QUERY-script.sql
@@ -67,4 +67,4 @@ order by count(*) desc
 
 -- 9. Het gemiddeld gebruikt budget per project.
 
--- 10. Welk team bevat meer als een werknemer, en hoeveel werkenemers zijn er in totaal per team.
+-- 10. Welke werknemer heeft meer dan 20 uur gepresteerd.
